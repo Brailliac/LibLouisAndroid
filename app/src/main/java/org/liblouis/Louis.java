a72643ac9ece71e91fcd6ed8917f3de54bd55f71
Luke <lukeneedham@outlook.com>
2020-06-07 13:27:36 +0200

    Reformat

--- app/src/main/java/org/liblouis/Louis.java
+++ app/src/main/java/org/liblouis/Louis.java
@@ -13,235 +13,241 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public final class Louis {
-  private final static String LOG_TAG = Louis.class.getName();
+    private final static String LOG_TAG = Louis.class.getName();
 
-  private final static String LIBRARY_NAME = "louis";
-  private final static String LIBRARY_VERSION;
-  public static native String getVersion ();
+    private final static String LIBRARY_NAME = "louis";
+    private final static String LIBRARY_VERSION;
 
-  static {
-    System.loadLibrary(LIBRARY_NAME);
+    public static native String getVersion();
 
-    LIBRARY_VERSION = getVersion();
-    Log.i(LOG_TAG, "liblouis version: " + LIBRARY_VERSION);
-  }
+    static {
+        System.loadLibrary(LIBRARY_NAME);
 
-  public static Object NATIVE_LOCK = new Object();
-  public static native void releaseMemory ();
-  public static native String getDataPath ();
-  public static native void setDataPath (String path);
-  private static native boolean compileTranslationTable (String table);
-  private static native void setLogLevel (char character);
-
-  public enum LogLevel {
-    ALL  ('A'),
-    DEBUG('D'),
-    INFO ('I'),
-    WARN ('W'),
-    ERROR('E'),
-    FATAL('F'),
-    OFF  ('O'),
-    ; // end of enumeration
-
-    private final char character;
-
-    LogLevel (char character) {
-      this.character = character;
+        LIBRARY_VERSION = getVersion();
+        Log.i(LOG_TAG, "liblouis version: " + LIBRARY_VERSION);
     }
 
-    public final char getCharacter () {
-      return character;
-    }
-  }
+    public static Object NATIVE_LOCK = new Object();
 
-  public static void setLogLevel (LogLevel level) {
-    setLogLevel(level.getCharacter());
-  }
+    public static native void releaseMemory();
 
-  private final static Object INITIALIZATION_LOCK = new Object();
-  private static Context currentContext = null;
-  private static File dataDirectory = null;
+    public static native String getDataPath();
 
-  private static void requireInitialization () {
-    synchronized (INITIALIZATION_LOCK) {
-      if (currentContext == null) {
-        throw new IllegalStateException("not initialized yet");
-      }
-    }
-  }
+    public static native void setDataPath(String path);
 
-  public static Context getContext () {
-    requireInitialization();
-    return currentContext;
-  }
+    private static native boolean compileTranslationTable(String table);
 
-  public static File getDataDirectory () {
-    requireInitialization();
-    return dataDirectory;
-  }
+    private static native void setLogLevel(char character);
 
-  private static SharedPreferences getSharedPreferences () {
-    return PreferenceManager.getDefaultSharedPreferences(currentContext);
-  }
+    public enum LogLevel {
+        ALL('A'),
+        DEBUG('D'),
+        INFO('I'),
+        WARN('W'),
+        ERROR('E'),
+        FATAL('F'),
+        OFF('O'),
+        ; // end of enumeration
 
-  private static void removeFile (File file) {
-    if (file.isDirectory()) {
-      file.setWritable(true, true);
+        private final char character;
 
-      for (String name : file.list()) {
-        removeFile(new File(file, name));
-      }
-    }
-
-    file.delete();
-  }
-
-  private static void extractAssets (AssetManager assets, String asset, File location) {
-    try {
-      String[] names = assets.list(asset);
-      boolean isDirectory = names.length > 0;
-      String path = location.getAbsolutePath();
-
-      if (isDirectory) {
-        if (!location.exists()) {
-          if (!location.mkdir()) {
-            Log.w(LOG_TAG, "directory not created: " + path);
-            return;
-          }
-        } else if (!location.isDirectory()) {
-          Log.w(LOG_TAG, "not a directory: " + path);
-          return;
+        LogLevel(char character) {
+            this.character = character;
         }
 
-        for (String name : names) {
-          extractAssets(assets, new File(asset, name).getPath(), new File(location, name));
+        public final char getCharacter() {
+            return character;
         }
-      } else {
-        InputStream input = assets.open(asset);
-        OutputStream output = new FileOutputStream(location);
-        byte[] buffer = new byte[0X1000];
+    }
 
-        for (int count; ((count = input.read(buffer)) > 0); ) {
-          output.write(buffer, 0, count);
+    public static void setLogLevel(LogLevel level) {
+        setLogLevel(level.getCharacter());
+    }
+
+    private final static Object INITIALIZATION_LOCK = new Object();
+    private static Context currentContext = null;
+    private static File dataDirectory = null;
+
+    private static void requireInitialization() {
+        synchronized (INITIALIZATION_LOCK) {
+            if (currentContext == null) {
+                throw new IllegalStateException("not initialized yet");
+            }
+        }
+    }
+
+    public static Context getContext() {
+        requireInitialization();
+        return currentContext;
+    }
+
+    public static File getDataDirectory() {
+        requireInitialization();
+        return dataDirectory;
+    }
+
+    private static SharedPreferences getSharedPreferences() {
+        return PreferenceManager.getDefaultSharedPreferences(currentContext);
+    }
+
+    private static void removeFile(File file) {
+        if (file.isDirectory()) {
+            file.setWritable(true, true);
+
+            for (String name : file.list()) {
+                removeFile(new File(file, name));
+            }
         }
 
-        input.close();
-        output.close();
-      }
-
-      location.setExecutable(isDirectory, false);
-      location.setWritable(false, false);
-      location.setReadable(true, false);
-    } catch (IOException exception) {
-      Log.e(LOG_TAG, "directory refresh error: " + exception.getMessage());
-    }
-  }
-
-  private static void extractAssets () {
-    AssetManager assets = currentContext.getAssets();
-
-    String name = "liblouis";
-    String oldName = name + ".old";
-    String newName = name + ".new";
-
-    File location = new File(dataDirectory, name);
-    File oldLocation = new File(dataDirectory, oldName);
-    File newLocation = new File(dataDirectory, newName);
-
-    removeFile(oldLocation);
-    removeFile(newLocation);
-    extractAssets(assets, name, newLocation);
-
-    synchronized (NATIVE_LOCK) {
-      location.renameTo(oldLocation);
-      newLocation.renameTo(location);
-
-      Log.d(LOG_TAG, "assets updated");
-      releaseMemory();
+        file.delete();
     }
 
-    removeFile(oldLocation);
-  }
+    private static void extractAssets(AssetManager assets, String asset, File location) {
+        try {
+            String[] names = assets.list(asset);
+            boolean isDirectory = names.length > 0;
+            String path = location.getAbsolutePath();
 
-  private static void updatePackageData () {
-    final SharedPreferences prefs = getSharedPreferences();
-    final File file = new File(currentContext.getPackageCodePath());
+            if (isDirectory) {
+                if (!location.exists()) {
+                    if (!location.mkdir()) {
+                        Log.w(LOG_TAG, "directory not created: " + path);
+                        return;
+                    }
+                } else if (!location.isDirectory()) {
+                    Log.w(LOG_TAG, "not a directory: " + path);
+                    return;
+                }
 
-    final String prefKey_size = "package-size";
-    final long oldSize = prefs.getLong(prefKey_size, -1);
-    final long newSize = file.length();
+                for (String name : names) {
+                    extractAssets(assets, new File(asset, name).getPath(), new File(location, name));
+                }
+            } else {
+                InputStream input = assets.open(asset);
+                OutputStream output = new FileOutputStream(location);
+                byte[] buffer = new byte[0X1000];
 
-    final String prefKey_time = "package-time";
-    final long oldTime = prefs.getLong(prefKey_time, -1);
-    final long newTime = file.lastModified();
+                for (int count; ((count = input.read(buffer)) > 0); ) {
+                    output.write(buffer, 0, count);
+                }
 
-    if ((newSize != oldSize) || (newTime != oldTime)) {
-      Log.d(LOG_TAG, "package size: " + oldSize + " -> " + newSize);
-      Log.d(LOG_TAG, "package time: " + oldTime + " -> " + newTime);
+                input.close();
+                output.close();
+            }
 
-      new Thread() {
-        @Override
-        public void run () {
-          Log.d(LOG_TAG, "begin extracting assets");
-          extractAssets();
-          Log.d(LOG_TAG, "end extracting assets");
-
-          SharedPreferences.Editor editor = prefs.edit();
-          editor.putLong(prefKey_size, newSize);
-          editor.putLong(prefKey_time, newTime);
-          editor.commit();
+            location.setExecutable(isDirectory, false);
+            location.setWritable(false, false);
+            location.setReadable(true, false);
+        } catch (IOException exception) {
+            Log.e(LOG_TAG, "directory refresh error: " + exception.getMessage());
         }
-      }.start();
-    }
-  }
-
-  public static void initialize (Context context) {
-    synchronized (INITIALIZATION_LOCK) {
-      if (currentContext != null) {
-        throw new IllegalStateException("already initialized");
-      }
-
-      currentContext = context;
-      dataDirectory = context.getDir(LIBRARY_NAME, Context.MODE_WORLD_READABLE);
-
-      setDataPath(dataDirectory.getAbsolutePath());
     }
 
-    updatePackageData();
-  }
+    private static void extractAssets() {
+        AssetManager assets = currentContext.getAssets();
 
-  public final static boolean compileTranslationTable (File table) {
-    synchronized (NATIVE_LOCK) {
-      return compileTranslationTable(table.getAbsolutePath());
+        String name = "liblouis";
+        String oldName = name + ".old";
+        String newName = name + ".new";
+
+        File location = new File(dataDirectory, name);
+        File oldLocation = new File(dataDirectory, oldName);
+        File newLocation = new File(dataDirectory, newName);
+
+        removeFile(oldLocation);
+        removeFile(newLocation);
+        extractAssets(assets, name, newLocation);
+
+        synchronized (NATIVE_LOCK) {
+            location.renameTo(oldLocation);
+            newLocation.renameTo(location);
+
+            Log.d(LOG_TAG, "assets updated");
+            releaseMemory();
+        }
+
+        removeFile(oldLocation);
     }
-  }
 
-  public final static boolean compileTranslationTable (TranslationTable table) {
-    return compileTranslationTable(table.getFile());
-  }
+    private static void updatePackageData() {
+        final SharedPreferences prefs = getSharedPreferences();
+        final File file = new File(currentContext.getPackageCodePath());
 
-  public static BrailleTranslation getBrailleTranslation (
-    TranslationTable table, CharSequence text
-  ) {
-    return new TranslationBuilder()
-              .setTranslationTable(table)
-              .setInputCharacters(text)
-              .setOutputLength(text.length() * 2)
-              .setAllowLongerOutput(true)
-              .newBrailleTranslation();
-  }
+        final String prefKey_size = "package-size";
+        final long oldSize = prefs.getLong(prefKey_size, -1);
+        final long newSize = file.length();
 
-  public static TextTranslation getTextTranslation (
-    TranslationTable table, CharSequence braille
-  ) {
-    return new TranslationBuilder()
-              .setTranslationTable(table)
-              .setInputCharacters(braille)
-              .setOutputLength(braille.length() * 3)
-              .setAllowLongerOutput(true)
-              .newTextTranslation();
-  }
+        final String prefKey_time = "package-time";
+        final long oldTime = prefs.getLong(prefKey_time, -1);
+        final long newTime = file.lastModified();
 
-  private Louis () {
-  }
+        if ((newSize != oldSize) || (newTime != oldTime)) {
+            Log.d(LOG_TAG, "package size: " + oldSize + " -> " + newSize);
+            Log.d(LOG_TAG, "package time: " + oldTime + " -> " + newTime);
+
+            new Thread() {
+                @Override
+                public void run() {
+                    Log.d(LOG_TAG, "begin extracting assets");
+                    extractAssets();
+                    Log.d(LOG_TAG, "end extracting assets");
+
+                    SharedPreferences.Editor editor = prefs.edit();
+                    editor.putLong(prefKey_size, newSize);
+                    editor.putLong(prefKey_time, newTime);
+                    editor.commit();
+                }
+            }.start();
+        }
+    }
+
+    public static void initialize(Context context) {
+        synchronized (INITIALIZATION_LOCK) {
+            if (currentContext != null) {
+                throw new IllegalStateException("already initialized");
+            }
+
+            currentContext = context;
+            dataDirectory = context.getDir(LIBRARY_NAME, Context.MODE_WORLD_READABLE);
+
+            setDataPath(dataDirectory.getAbsolutePath());
+        }
+
+        updatePackageData();
+    }
+
+    public final static boolean compileTranslationTable(File table) {
+        synchronized (NATIVE_LOCK) {
+            return compileTranslationTable(table.getAbsolutePath());
+        }
+    }
+
+    public final static boolean compileTranslationTable(TranslationTable table) {
+        return compileTranslationTable(table.getFile());
+    }
+
+    public static BrailleTranslation getBrailleTranslation(
+            TranslationTable table, CharSequence text
+    ) {
+        return new TranslationBuilder()
+                .setTranslationTable(table)
+                .setInputCharacters(text)
+                .setOutputLength(text.length() * 2)
+                .setAllowLongerOutput(true)
+                .newBrailleTranslation();
+    }
+
+    public static TextTranslation getTextTranslation(
+            TranslationTable table, CharSequence braille
+    ) {
+        return new TranslationBuilder()
+                .setTranslationTable(table)
+                .setInputCharacters(braille)
+                .setOutputLength(braille.length() * 3)
+                .setAllowLongerOutput(true)
+                .newTextTranslation();
+    }
+
+    private Louis() {
+    }
 }
